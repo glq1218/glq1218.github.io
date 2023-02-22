d379610bdde28f9b7e12b8395efc3e1a45535ef9
glq1218 <glq05201218@163.com>
2023-02-22 18:04:02 +0800

    delpoy

---
+++ assets/chunks/virtual_search-data.48198de3.js
@@ -0,0 +1,14 @@
+const t={map:'[{"java":["1.0"],"中文":["1.1"],"tada":["2.0"],"this":["2.1"],"壁纸分享":["3.0"],"vpteampage":["4.0"],"vue":["5.0"],"分享关于vue的知识":["5.1"],"vite":["6.0"]},{"笔记":["1.0","5.0"],"test":["2.0"],"is":["2.1"],"home":["3.0"],"跳转到根目录的index":["3.0"],"md":["3.0"],"foo":["3.0"],"vpteampagetitle":["4.0"],"template":["4.0"]},{"这是一篇关于":["1.0"],"a":["2.1"],"跳转到":["3.0"],"文件夹的":["3.0"],"index":["3.0"],"title":["4.0"],"我的团队":["4.0"]},{"test":["2.1"],"html":["3.0"],"heading":["3.0"],"lead":["4.0"]},{"的笔记":["1.0"],"测试":["2.1"],"的特定标题位置":["3.0"],"vitepress的开发由国际团队":["4.0"],"其中一些人已选择在下面进行专题介绍":["4.0"]},{"1":["1.0"],"bar":["3.0"],"three":["3.0"],"你可以忽略扩展名":["3.0"]},{"测试":["1.0"],"具体文件可以使用":["3.0"],"结尾":["3.0"],"推荐":["3.0"],"vpteammembers":["4.0"],"size":["4.0"],"small":["4.0"]},{"four":["3.0"],"也可以用":["3.0"],"members":["4.0"]},{"少年歌行":["3.0"],"sngx":["3.0"],"png":["3.0"]}]'},e={"1.0":{t:"java-笔记",p:`
+这是一篇关于 Java 的笔记
+
+
+测试
+
+`,l:"java/index.html",a:"java-笔记"},"1.1":{t:"中文",p:"",l:"java/index.html#中文",a:"中文"},"2.0":{t:"tada",p:`test
+`,l:"java/test.html",a:"tada"},"2.1":{t:"this-is-a-test",p:`测试
+`,l:"java/test.html#this-is-a-test",a:"this-is-a-test"},"3.0":{t:"壁纸分享",p:`&lt;!-- Home 跳转到根目录的index.md
+foo 跳转到 foo 文件夹的 index.html
+foo h ...`,l:"share/wallpaper.html",a:"壁纸分享"},"4.0":{t:"vpteampage",p:`&lt;VPTeamPageTitle&gt;
+&lt;template #title&gt;
+我的团队
+&lt;/temp ...`,l:"team.html",a:"vpteampage"},"5.0":{t:"vue-笔记",p:"",l:"vue/index.html",a:"vue-笔记"},"5.1":{t:"分享关于vue的知识",p:"",l:"vue/index.html#分享关于vue的知识",a:"分享关于vue的知识"},"6.0":{t:"vite",p:"",l:"vue/vite.html",a:"vite"}},a={previewLength:62,buttonLabel:"搜索",placeholder:"请输入关键字"},l={INDEX_DATA:t,PREVIEW_LOOKUP:e,Options:a};export{l as default};
--- assets/vue_vite.md.515f3e93.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as t,c as a,a as o}from"./app.4a40b79a.js";const v=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"vue/vite.md"}'),s={name:"vue/vite.md"},n=o("p",null,"#vite",-1),r=[n];function c(i,_,d,p,l,m){return t(),a("div",null,r)}const f=e(s,[["render",c]]);export{v as __pageData,f as default};

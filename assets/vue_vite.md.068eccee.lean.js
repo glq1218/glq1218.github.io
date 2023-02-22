7cd7d34fc221da82743d75ed2740164728182952
glq1218 <glq05201218@163.com>
2023-02-22 16:44:25 +0800

    deploy

---
+++ assets/vue_vite.md.068eccee.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,o as t,c as a,a as o}from"./app.a70e8e3e.js";const v=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"vue/vite.md"}'),s={name:"vue/vite.md"},n=o("p",null,"#vite",-1),r=[n];function c(i,_,d,p,l,m){return t(),a("div",null,r)}const f=e(s,[["render",c]]);export{v as __pageData,f as default};
